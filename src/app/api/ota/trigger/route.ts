ecf36c65a7de5f1bd66e6875c26ba8865e2ca368
aldo <ewaldozeno@gmail.com>
2026-05-19 07:26:23 +0800

    feat: update MQTT broker configuration to use secure connection

--- src/app/api/ota/trigger/route.ts
+++ src/app/api/ota/trigger/route.ts
@@ -5,8 +5,8 @@ import clientPromise from "@/lib/db";
 import { ObjectId } from "mongodb";
 import mqtt from "mqtt";
 
-const EMQX_BROKER = "mqtt://pa120029.ala.asia-southeast1.emqxsl.com";
-const EMQX_PORT = 1883;
+const EMQX_BROKER = "mqtts://pa120029.ala.asia-southeast1.emqxsl.com";
+const EMQX_PORT = 8883;
 const EMQX_USER = "aldoo";
 const EMQX_PASS = "Ew4ld012345";
 
@@ -53,6 +53,7 @@ export async function POST(req: Request) {
       password: EMQX_PASS,
       clientId: `dashboard-${Date.now()}`,
       reconnectPeriod: 5000,
+      rejectUnauthorized: false,
     });
 
     return new Promise<NextResponse>((resolve) => {
